dfe40dca6bbbcc821621e64a30a4379d3caf6b23
thomas <thomas@thomas-W150ER>
2013-06-06 17:37:47 +0200

    Improving Color-Class: Better implementation, more functions, adding Color-List

--- include/Color.hpp
+++ include/Color.hpp
@@ -8,42 +8,95 @@
 #ifndef SSDL_COLOR_HPP
 #define	SSDL_COLOR_HPP
 
+#include <stdint.h>
+
 namespace SDL {
 
     class Color {
     public:
-        explicit Color(const int Red = 0, const int Green = 0, const int Blue = 0, const int Alpha = 255);
-        //        Color(const Color& orig);
-        //        virtual ~Color();
 
-        void SetColor(const int Red = 0, const int Green = 0, const int Blue = 0, const int Alpha = 255);
+        explicit Color(const uint8_t Red = 0, const uint8_t Green = 0, const uint8_t Blue = 0, const uint8_t Alpha = 255);
 
-        // XXX: SetFunctions with Value correcting or uint8
+        void SetColor(const uint8_t Red = 0, const uint8_t Green = 0, const uint8_t Blue = 0, const uint8_t Alpha = 255);
 
-        int GetRed() {
-            return this->Red;
+        void SetRed(const uint8_t Value) {
+            this->RedColor = Value;
         }
 
-        int GetGreen() {
-            return this->Green;
+        void SetGreen(const uint8_t Value) {
+            this->GreenColor = Value;
         }
 
-        int GetBlue() {
-            return this->Blue;
+        void SetBlue(const uint8_t Value) {
+            this->BlueColor = Value;
         }
 
-        int GetAlpha() {
-            return this->Alpha;
+        void SetAlpha(const uint8_t Value) {
+            this->AlphaColor = Value;
+        }
+
+        uint8_t GetRed() {
+            return this->RedColor;
+        }
+
+        uint8_t GetGreen() {
+            return this->GreenColor;
+        }
+
+        uint8_t GetBlue() {
+            return this->BlueColor;
+        }
+
+        uint8_t GetAlpha() {
+            return this->AlphaColor;
         }
 
     private:
 
-        int Red;
-        int Green;
-        int Blue;
-        int Alpha;
+        uint8_t RedColor;
+        uint8_t GreenColor;
+        uint8_t BlueColor;
+        uint8_t AlphaColor;
+
+    public:
 
     };
+
+    namespace ColorList {
+
+        // White -> Black
+
+        const SDL::Color White = SDL::Color(255, 255, 255);
+        const SDL::Color LightGrey = SDL::Color(192, 192, 192);
+        const SDL::Color Grey = SDL::Color(128, 128, 128);
+        const SDL::Color DarkGrey = SDL::Color(64, 64, 64);
+        const SDL::Color Black = SDL::Color(0, 0, 0);
+
+        // Red
+
+        const SDL::Color LightRed = SDL::Color(255, 128, 128);
+        const SDL::Color Red = SDL::Color(255, 0, 0);
+        const SDL::Color DarkRed = SDL::Color(64, 0, 0);
+
+        // Green
+
+        const SDL::Color LightGreen = SDL::Color(128, 255, 128);
+        const SDL::Color Green = SDL::Color(0, 255, 0);
+        const SDL::Color DarkGreen = SDL::Color(0, 64, 0);
+
+        // Blue
+
+        const SDL::Color LightBlue = SDL::Color(128, 128, 255);
+        const SDL::Color Blue = SDL::Color(0, 0, 255);
+        const SDL::Color DarkBlue = SDL::Color(0, 0, 64);
+
+        // Other
+
+        const SDL::Color Yellow = SDL::Color(255, 255, 0);
+        const SDL::Color Pink = SDL::Color(255, 0, 255);
+        const SDL::Color Turquoise = SDL::Color(0, 255, 255);
+
+    }
 
 }
 
--- src/Color.cpp
+++ src/Color.cpp
@@ -5,26 +5,24 @@
  * Created on 5. Juni 2013, 19:56
  */
 
+#include <stdint.h>
+
+#include <SDL/SDL_stdinc.h>
+
 #include "../include/Color.hpp"
 
 namespace SDL {
 
-    Color::Color(const int Red, const int Green, const int Blue, const int Alpha) {
+    Color::Color(const uint8_t Red, const uint8_t Green, const uint8_t Blue, const uint8_t Alpha) {
         this->SetColor(Red, Green, Blue, Alpha);
     }
 
-    //    Color::Color(const Color& orig) {
-    //    }
-    //
-    //    Color::~Color() {
-    //    }
+    void Color::SetColor(const uint8_t Red, const uint8_t Green, const uint8_t Blue, const uint8_t Alpha) {
 
-    void Color::SetColor(const int Red, const int Green, const int Blue, const int Alpha) {
-
-        this->Red = Red;
-        this->Green = Green;
-        this->Blue = Blue;
-        this->Alpha = Alpha;
+        this->SetRed(Red);
+        this->SetGreen(Green);
+        this->SetBlue(Blue);
+        this->SetAlpha(Alpha);
     }
 
 }
